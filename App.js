3b1e03e70c58a293880ac5276bc2ad5c4d7a364e
amang <amangupta6121998@gmail.com>
2021-08-05 21:52:49 +0530

    updated : script for displaying calculated result

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-console.log("stock analyzer");
 const purcashePrice = document.getElementById("purchase_price");
 const stockQuantity = document.getElementById("quantity");
 const currentPrice = document.getElementById("current_price");
 const check = document.getElementById("btn");
+const ReslutPage = document.getElementById("displayResult");
+const ShowResultInNum = document.getElementById("resultInNum");
+const ShowResultInPercent = document.getElementById("resultInPercentage");
+const closeBtn = document.getElementById("close");
 let purchase_price = 0;
 let stock_quanitiy = 0;
 let current_price = 0;
 
 const AnalyseStock = (purchase_price, stock_quanitiy, current_price) => {
-  //   console.log(purchase_price, stock_quanitiy, current_price);
-
   const TotalAmountSpent = purchase_price * stock_quanitiy;
 
   const CurrentMarketValue = current_price * stock_quanitiy;
@@ -17,19 +18,35 @@ const AnalyseStock = (purchase_price, stock_quanitiy, current_price) => {
   if (TotalAmountSpent > CurrentMarketValue) {
     const Loss = TotalAmountSpent - CurrentMarketValue;
     const LossInPercent = (Loss / TotalAmountSpent) * 100;
-    console.log(Loss, LossInPercent);
+
+    ReslutPage.style.display = "grid";
+    ReslutPage.style.color = "red";
+    ReslutPage.style.backgroundImage =
+      "url(https://media.giphy.com/media/lSzQjkthGS1gc/giphy.gif)";
+    ShowResultInNum.innerHTML = `You lost  ₹ ${Loss} `;
+    ShowResultInPercent.innerHTML = ` Your total loss is  ${LossInPercent}%`;
   } else {
     const Profit = CurrentMarketValue - TotalAmountSpent;
     const ProfitInPercent = (Profit / TotalAmountSpent) * 100;
-    console.log(Profit, ProfitInPercent);
+    ReslutPage.style.backgroundImage =
+      "url(https://media.giphy.com/media/JtBZm3Getg3dqxK0zP/giphy.gif)";
+    ReslutPage.style.display = "grid";
+    ReslutPage.style.color = "lime";
+
+    ShowResultInNum.innerHTML = `You gained  ₹ ${Profit}`;
+    ShowResultInPercent.innerHTML = ` Your total profit is  ${ProfitInPercent}%`;
   }
 };
 
+closeBtn.addEventListener("click", () => {
+  ReslutPage.style.display = "none";
+});
+
 check.addEventListener("click", () => {
-  if (purchase_price >= 0 && stock_quanitiy >= 0 && current_price >= 0) {
+  if (purchase_price > 0 && stock_quanitiy > 0 && current_price > 0) {
     AnalyseStock(purchase_price, stock_quanitiy, current_price);
   } else {
-    alert("Numbers cant not be -ve");
+    alert("Invalid Number");
   }
 });
 
